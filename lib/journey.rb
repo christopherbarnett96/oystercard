def159987758ded81cfb986b8c0909eab5ecd981
Topher <topherb99@gmail.com>
2019-10-24 15:20:29 +0100

    journey class created, tests passing. No integration of classes.

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,13 +1,37 @@
 
 
 class Journey
+  PENALTY_FARE = 6
+  FARE = 1
 
   attr_accessor :entry_station
+  attr_reader :exit_station
 
-  def initialize(entry_station = nil)
+  def initialize(entry_station = nil, exit_station = nil)
     @entry_station = entry_station
+    @exit_station = exit_station
   end
 
+  def complete?
+    if !!entry_station && !! exit_station
+      true
+    else
+      false
+    end
+  end
+
+  def fare
+    if complete?
+      FARE
+    else
+      PENALTY_FARE
+    end
+  end
+
+  def finish(station)
+    @exit_station = station
+    self
+  end
 
 
   def in_journey?
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,7 +2,60 @@ require 'journey'
 
 describe Journey do
 
-  it "Knows whether it's in a journey or not" do
+  let(:station) { double :station, name: "aldgate", zone: 1 }
+
+  it "Knows if it's not in_journey" do
     expect(subject).to_not be_in_journey
   end
+
+  it 'knows if a journey is not complete' do
+    expect(subject).to_not be_complete
+  end
+
+  it 'has a penalty fare as default' do
+    expect(subject.fare).to eq Journey::PENALTY_FARE
+  end
+
+  it 'returns itself when exiting a journey' do
+    expect(subject.finish(station)).to eq subject
+  end
+
+  context 'given an entry station' do
+    subject {described_class.new(station)}
+
+    it 'has an entry station' do
+      expect(subject.entry_station).to eq station
+    end
+
+    it 'Knows if it is in_journey' do
+      expect(subject).to be_in_journey
+    end
+
+    it 'returns a penalty fare if no exit station given' do
+      expect(subject.fare).to eq Journey::PENALTY_FARE
+    end
+
+    context 'given an exit station' do
+      let(:exit_station) { double :exit_station }
+
+      before do
+        subject.finish(exit_station)
+      end
+
+      it 'calculates a fare' do
+        expect(subject.fare).to eq 1
+      end
+
+      it 'knows if a journey is complete' do
+        expect(subject).to be_complete
+      end
+    end
+
+
+  end
+
+
+
+
+
 end
